8c622f2a6947882874999fd9904a0ee62adfa14a
martinoremonje <m.monjeorellana@gmail.com>
2024-03-07 13:10:33 -0300

    subir desafio guiado 02

---
+++ assets/index.js
@@ -0,0 +1,21 @@
+let thediv = document.getElementById("theDiv");
+
+const multiplicarYFactorial = () => {
+    let num = parseInt(prompt("Ingrese numero mayor a 0 y menor a 20"))
+    if(num < 20 && num > 0){
+        let factorial = 1;
+        for (let i = 1; i <= 10; i++) {
+        let res = i * num;
+        thediv.innerHTML += `${i} x ${num} = ${res} <br>`
+        console.log(`${i} * ${num} = ${res}`)         
+        }        
+        for (let i = 1; i <= num; i++) {
+        factorial *= i;   
+        console.log(`Factorial de ${i} es: ${factorial}`)
+        }
+    } else {
+        alert('ingrese numero menor a 20');  
+        multiplicarYFactorial();      
+      }
+}
+multiplicarYFactorial();
